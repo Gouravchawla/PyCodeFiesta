76b71609d2f6adb309ce69f5d2005cf2b7b06487
Gourav Chawla <gouravchawla@live.com>
2015-09-25 15:37:28 +0530

    Better Version

--- findingThePercentage.py
+++ findingThePercentage.py
@@ -1,9 +1,10 @@
 """HackerRank link : https://www.hackerrank.com/challenges/finding-the-percentage"""
 stuRecords = {}
-n = eval(input())
-for i in range(n):
+#n = eval(input())
+for i in range(eval(input())):
     inp = input().split()
-    stuRecords[inp[0]] = inp[1:4]
-searchKey = input()
-result = stuRecords[searchKey]
-print(format((eval(result[0])+eval(result[1])+eval(result[2]))/3,'.2f'))
+    stuRecords[inp[0]] = sum(map(float,inp[1:]))/3
+#searchKey = input()
+print("%.2f" %stuRecords[input()])
+#print("%.2f" %result)
+#print(format((eval(result[0])+eval(result[1])+eval(result[2]))/3,'.2f'))
